ceffdc43090e379ec41cfd0e54da7b9622ab390d
rachmizard <rachmizard11072000@gmail.com>
2021-02-28 00:56:00 +0700

    styling and grid for component hero

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 import Header from "parts/Header";
 
+import landingPage from "mocks/landingPage.json";
+import Hero from "parts/Hero";
+
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero}></Hero>
       </>
     );
   }
