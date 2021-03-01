b476362236572054f06477ff317a6c0f67f3ebf7
rachmizard <rachmizard11072000@gmail.com>
2021-03-01 13:35:24 +0700

    implement most picked component (part, sytle)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 
 import Header from "parts/Header";
+import MockPicked from "parts/MostPicked";
 
 import landingPage from "mocks/landingPage.json";
 import Hero from "parts/Hero";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero}></Hero>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}></Hero>
+        <MockPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        ></MockPicked>
       </>
     );
   }
