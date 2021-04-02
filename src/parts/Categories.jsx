c3e37cf8f1bf936bc83dd87e90193b4cddcc05a1
rachmizard <rachmizard11072000@gmail.com>
2021-04-03 02:05:19 +0700

    install react reveal & apply to the parts element

--- src/parts/Categories.jsx
+++ src/parts/Categories.jsx
@@ -1,3 +1,4 @@
+import Fade from "react-reveal/Fade";
 import Button from "elements/Button";
 import React from "react";
 
@@ -5,50 +6,54 @@ export default function Categories({ data }) {
   return data.map((category, index1) => {
     return (
       <section className="container" key={index1}>
-        <h4 className="mb-3 font-weight-medium">{category.name}</h4>
-        <div className="container-grid">
-          {category.items.length === 0 ? (
-            <div className="row">
-              <div className="col-auto align-items-center">
-                There is no property at this categori.
-              </div>
-            </div>
-          ) : (
-            category.items.map((item, index2) => {
-              return (
-                <div key={index2} className="item column-3 row-1">
-                  <div className="card">
-                    {item.isPopular && (
-                      <div className="tag">
-                        Popular{" "}
-                        <span className="font-weight-light">Choice</span>
-                      </div>
-                    )}
-                    <figure className="img-wrapper" style={{ height: 180 }}>
-                      <img
-                        src={item.imageUrl}
-                        alt={item.name}
-                        className="img-cover"
-                      />
-                    </figure>
-                    <div className="meta-wrapper">
-                      <Button
-                        type="link"
-                        href={`properties/${item._id}`}
-                        className="streched-link d-block text-grey-900"
-                      >
-                        <h5 className="h4">{item.name}</h5>
-                      </Button>
-                      <span className="text-gray-500">
-                        {item.city}, {item.country}
-                      </span>
-                    </div>
-                  </div>
+        <Fade bottom>
+          <h4 className="mb-3 font-weight-medium">{category.name}</h4>
+          <div className="container-grid">
+            {category.items.length === 0 ? (
+              <div className="row">
+                <div className="col-auto align-items-center">
+                  There is no property at this categori.
                 </div>
-              );
-            })
-          )}
-        </div>
+              </div>
+            ) : (
+              category.items.map((item, index2) => {
+                return (
+                  <div key={index2} className="item column-3 row-1">
+                    <Fade bottom delay={300 * index2}>
+                      <div className="card">
+                        {item.isPopular && (
+                          <div className="tag">
+                            Popular{" "}
+                            <span className="font-weight-light">Choice</span>
+                          </div>
+                        )}
+                        <figure className="img-wrapper" style={{ height: 180 }}>
+                          <img
+                            src={item.imageUrl}
+                            alt={item.name}
+                            className="img-cover"
+                          />
+                        </figure>
+                        <div className="meta-wrapper">
+                          <Button
+                            type="link"
+                            href={`properties/${item._id}`}
+                            className="streched-link d-block text-grey-900"
+                          >
+                            <h5 className="h4">{item.name}</h5>
+                          </Button>
+                          <span className="text-gray-500">
+                            {item.city}, {item.country}
+                          </span>
+                        </div>
+                      </div>
+                    </Fade>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </Fade>
       </section>
     );
   });
